3a8f92e771c643d161eccfc8e710d5de67a47383
lola-source <adagaladolores@gmail.com>
2022-09-05 18:55:09 +0000

    01

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-'use strict';
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const arg in args) {
-  console.log(args[arg]);
+const ls = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i in ls) {
+  console.log(ls[i]);
 }
