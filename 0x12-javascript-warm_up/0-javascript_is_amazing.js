189cb9414e4e243c3cb196fd9bf22b07332f5161
lola-source <adagaladolores@gmail.com>
2022-09-11 13:51:36 +0000

    merdge

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
+<<<<<<< Updated upstream
 const myVar = 'JavaScript is amazing';
+=======
+
+'use strict';
+const myVar = 'Javascript is amazing';
+>>>>>>> Stashed changes
 console.log(myVar);
